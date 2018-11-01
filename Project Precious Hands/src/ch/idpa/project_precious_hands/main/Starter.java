2b6f86d0e8f541e5e07f6596816970c7362e6730
olive <olive@ThinkPad-Oliver>
2018-11-01 08:52:05 +0100

    Basic project structure + FXML

--- Project Precious Hands/src/ch/idpa/project_precious_hands/main/Starter.java
+++ Project Precious Hands/src/ch/idpa/project_precious_hands/main/Starter.java
@@ -5,17 +5,38 @@
  */
 package ch.idpa.project_precious_hands.main;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
 /**
  *
  * @author chris
  */
-public class Starter {
+public class Starter extends Application{
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        Image icon = new Image("resources/Precious_Logo.png");
+        System.out.println(""+icon.toString());
+        Parent root = FXMLLoader.load(getClass().getResource("View/Login.fxml"));
+
+        Scene scene = new Scene(root);
+        
+        stage.setTitle("Precious Hands");
+        stage.getIcons().add(icon);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
 }
